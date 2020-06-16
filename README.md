6851ea2a340423e4e3cc23f4d75f1dd1564889ca
Buu Nguyen <buunguyen@gmail.com>
2020-06-16 12:40:41 -0700

    Clean up

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
-# spotify-importer
+## Spotify Track importer
 
-### Generate CSV file from a directory
+Script to import offline music library to Spotify.
+Require Node 14+.
 
-node ./index.js generate-csv -i /Users/me/Music -o ./songs.csv -f "\d+|_+|\[(.+)\]"
+### Generate track CSV file
 
-### Import songs to Spotify
+This script parses a directory and creates a CSV file of all tracks. The output CSV file has 4 columns: track name, artist, playlists (separated by `|`) and the Spotify URI (empty and you don't have to bother).
 
-node ./index.js import-songs -i ./songs.csv -u <p;ostify username> -t <spotify token> -p <default playlist>
+Run this:
+
+```
+node ./index.js generate-csv -i <directory> -o <csv file> -f <optional filter: regexp to clean up file names>
+```
+
+You can modify the output CSV file manually like changing playlists, cleaning up filenames further etc.
+
+### Import tracks from CSV file
+
+This script uses the Spotify Web API to create playlists (if not exist) and add the songs to those playlists. You need to create a Spotify App to get the Client ID and Client Secret. Also, add a Redirect Uri in the app settings. It could be anything like "https://localhost:8080/callback". The server doesn't have to exist, just need something to fill in the param below and make sure it matches the one in app settings. Finally, you need the Spotify user ID, which you can find in the Spotify Profile page.
+
+Run this and follow the instructions:
+
+```
+node ./index.js import-tracks -i <csv file> -c <client id> -s <client secret> -r <redirect uri> -u <user id>
+```
+
+Copy the 'Refresh Token' output from this run. If you need to re-run this script, do this, it will be faster:
+
+```
+node ./index.js import-tracks -i <csv file> -c <client id> -s <client secret> -r <redirect uri> -u <user id> -f <refresh token>
+```
+
+You can also add a `-p <default playlist`. Tracks are always imported to this playlist (default value if not provided is 'imported'). That way you have a single place to check all the imported tracks. If you don't want it later, you can delete it on Spotify after import.
+
+Import failures are written in a CSV file with the `.failed.csv` suffix. Clean up that file and use it as the input for re-run. Or you might have to manually import those if Spotify Search API doesn't work for those tracks.
+
+Tracks which were found are written to a CSV file with the `.found.csv` suffix. Use it to get the Spotify URI if you need it for some reason.
+
+If the script fails before writing the `.failed.csv`, try re-running. Spotify API fails for several reasons including rate limit or a permission hasn't taken effect (probably due to poor data consistency).
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import mm from 'music-metadata';
 
 const {createArrayCsvWriter: createCsvWriter} = csvWriter;
 
+/// Command line parsers
 const options = yargs
   .command('generate-csv', 'Generate a track CSV file from a directory', (yargs) => {
     return yargs
@@ -21,34 +22,41 @@ const options = yargs
   .command('import-tracks', 'Import tracks from a CSV file to Spotify', (yargs) => {
     return yargs
       .option('i', {alias: 'input', describe: 'Input CSV file', type: 'string', demandOption: true})
-      .option('c', {alias: 'clientId', describe: 'Spotify app client ID', type: 'string', demandOption: true})
-      .option('s', {alias: 'clientSecret', describe: 'Spotify app client secret', type: 'string', demandOption: true})
-      .option('r', {alias: 'redirectUri', describe: 'Redirect URL (must match the one specified in Spotify)', type: 'string', demandOption: true})
-      .option('u', {alias: 'username', describe: 'Spotify username', type: 'string', demandOption: false})
-      .option('f', {alias: 'refreshToken', describe: 'Access token (if already have it)', type: 'string', demandOption: false})
+      .option('c', {alias: 'clientId', describe: 'Spotify App Client ID', type: 'string', demandOption: true})
+      .option('s', {alias: 'clientSecret', describe: 'Spotify App Client Secret', type: 'string', demandOption: true})
+      .option('r', {alias: 'redirectUri', describe: 'Spotify Auth Redirect URI (must match the one specified in Spotify)', type: 'string', demandOption: true})
+      .option('u', {alias: 'userId', describe: 'Spotify user ID', type: 'string', demandOption: true})
+      .option('f', {alias: 'refreshToken', describe: 'Refresh token (if already have it)', type: 'string', demandOption: false})
       .option('p', {alias: 'defaultPlaylist', describe: 'Default playlist', type: 'string', demandOption: false})
   }, importTracks)
   .argv;
+
+/// Exception handling
+process.on('uncaughtException', (err) => logError(err));
+process.on('unhandledRejection', (reason, promise) => logError(reason));
+function logError(err) {
+  console.log('Unexpected error. Check the data and try re-run the script.')
+  console.log(err);
+}
 
 ///=== Task 1: generate CSV
 async function generateCsv({input, output, filter = ''}) {
   const csvWriter = createCsvWriter({path: output});
   const files = await fs.readdir(input);
-  const rows = [];
-
-  for (const file of files) {
+  const rows = await Promise.all(files.map(async (file) => {
     const {trackName, artist, playlists} = await parseMusicFile({filename: file, filter, dir: input});
-    rows.push([trackName, artist, playlists, '']);
-  }
-
+    return [trackName, artist, playlists, ''] ;
+  }));
   await csvWriter.writeRecords(rows);
 }
 
 async function parseMusicFile({filename, dir, filter}) {
-  const fullpath = path.join(dir, filename);
-  let trackName, artist, playlists;
+  let trackName = '';
+  let artist = '';
+  let playlists = '';
 
   try {
+    const fullpath = path.join(dir, filename);
     const metadata = await mm.parseFile(fullpath);
     trackName = metadata.common.title;
     artist = ((metadata.common.artist || metadata.common.artists[0]) || '').split(/[,/]+/)[0].trim();
@@ -61,63 +69,66 @@ async function parseMusicFile({filename, dir, filter}) {
   if (!artist) {
     try {
       artist = path.parse(filename).name.split('-')[1].trim();
-    } catch (ignored) {
-      artist = '';
-    }
+    } catch (ignored) {}
   }
-  if (!playlists) {
-    playlists = '';
-  }
+
   return {trackName, artist, playlists};
 }
 
 ///=== Task 2: import tracks
 async function importTracks(argv) {
-  const {rows, tracks, artists, playlists, uris} = await parseSource(argv);
+  const {tracks, artists, playlists, uris} = await parseSource(argv);
   const api = await auth(argv);
 
-  // Put tracks into playlist groups
-  const groups = {};
-  const failures = [];
+  // Find tracks and populate the the playlist groups: mapping between <playlist> -> <track 1> <track 2> <track n>
+  const playlistGroups = {};
+  const foundTracks = [];
+  const failedTracks = [];
+
+  console.log('Finding tracks, gonna take a while if there are many tracks...');
   for (let i = 0; i < tracks.length; i++) {
+    // If URI is supplied (e.g. by previous run), use it, if not, find the track on Spotify
     if (!uris[i]) {
       const track = await findTrack({api, track: tracks[i], artist: artists[i]});
       if (!track) {
-        failures.push([tracks[i], artists[i], playlists[i].join('|'), '']);
+        failedTracks.push([tracks[i], artists[i], playlists[i].join('|'), '']);
         continue;
       }
-
-      uris[i] = rows[i][3] = track.uri;
+      uris[i] = track.uri;
     }
-
+    foundTracks.push([tracks[i], artists[i], playlists[i].join('|'), uris[i]]);
     for (const playlist of playlists[i]) {
-      groups[playlist] = (groups[playlist] || []).concat(uris[i]);
+      playlistGroups[playlist] = (playlistGroups[playlist] || []).concat(uris[i]);
     }
   }
 
-  // Create playlist (if not exist) and add tracks
-  const {body: {items: onlinePlaylists}} = await api.getUserPlaylists(argv.username);
-  for (const playlistName of Object.keys(groups)) {
-    console.log(`Importing playlist '${playlistName}': ${groups[playlistName].length}...`);
-    let onlinePlaylist = onlinePlaylists.find((pl) => pl.name === playlistName);
-    if (!onlinePlaylist) {
+  // Create playlists, if not exist, and add tracks to them
+  const {body: {items: spotifyPlaylists}} = await api.getUserPlaylists(argv.userId);
+  for (const playlistName of Object.keys(playlistGroups)) {
+    console.log(`Importing playlist '${playlistName}': ${playlistGroups[playlistName].length}...`);
+    let spotifyPlaylist = spotifyPlaylists.find((pl) => pl.name === playlistName);
+    if (!spotifyPlaylist) {
+      // Playlist not exist on Spotify yet, create it now
       console.log(`Creating playlist '${playlistName}'...`)
-      onlinePlaylist = (await api.createPlaylist(argv.username, playlistName, {public: false})).body;
+      spotifyPlaylist = (await api.createPlaylist(argv.userId, playlistName, {public: false})).body;
     }
 
-    // If more than 100, throw 400, thus, chunk first
-    for (const chunk of chunkArray(groups[playlistName], 100)) {
-      await api.addTracksToPlaylist(onlinePlaylist.id, chunk);
+    // The API only allows a number of tracks per request, so chunk if the playlist is big
+    for (const chunkOfTracks of chunkArray(playlistGroups[playlistName], 100)) {
+      await api.addTracksToPlaylist(spotifyPlaylist.id, chunkOfTracks);
     }
   }
 
-  // Write file with URIs so it could be the input for the next run (faster)
-  createCsvWriter({path: path.parse(argv.input).name + '.done.cvs'}).writeRecords(rows);
+  // Write file with URIs for found tracks
+  createCsvWriter({path: path.parse(argv.input).name + '.found.csv'}).writeRecords(foundTracks);
 
-  // Write all failures to a file so it could be input for a re-run (after manually update)
-  if (failures.length) {
-    console.log(`Failures: ${failures.length}`);
-    createCsvWriter({path: path.parse(argv.input).name + '.failed.csv'}).writeRecords(failures);
+  // Write all failed tracks to a file so it could be input for a re-run (after manually update)
+  if (failedTracks.length) {
+    const failedFilePath = path.parse(argv.input).name + '.failed.csv';
+    console.log(`Couldn't import ${failedTracks.length} tracks. Details are in '${failedFilePath}'. Update that file and use it as input for the next run.`);
+    createCsvWriter({path: failedFilePath}).writeRecords(failedTracks);
+  } else {
+    console.log('ALL DONE 🎉🎊🥳! All tracks were imported')
   }
 }
 
@@ -127,10 +138,12 @@ async function findTrack({api, track, artist, playlists}) {
     query += ` artist:${artist}`;
   }
   const {body: {tracks: {items}}} = await api.searchTracks(query);
+
+  // TODO: probably better if allow to choose among the options
   return items[0];
 }
 
-async function parseSource({input, username, defaultPlaylist = 'imported'}) {
+async function parseSource({input, defaultPlaylist = 'imported'}) {
   const fileData = await fs.readFile(input);
   const rows = await new Promise((resolve, reject) => {
     csvParse(fileData, {}, (err, rows) => resolve(rows));
@@ -139,17 +152,19 @@ async function parseSource({input, username, defaultPlaylist = 'imported'}) {
   const artists = rows.map((row) => row[1].trim());
   const playlists = rows.map((row) => row[2].trim().split('|').filter((playlist) => !!playlist).concat(defaultPlaylist));
   const uris = rows.map((row) => row[3].trim());
-  return {rows, tracks, artists, playlists, uris};
+  return {tracks, artists, playlists, uris};
 }
 
 async function auth({clientId, clientSecret, redirectUri, refreshToken}) {
-  const api = new SpotifyWebApi({
-    clientId,
-    clientSecret,
-    redirectUri,
-  });
+  const api = new SpotifyWebApi({clientId, clientSecret, redirectUri});
 
-  if (!refreshToken) {
+  if (refreshToken) {
+    // If refresh token is provided, set it and get the access token
+    api.setRefreshToken(refreshToken);
+    const data = await api.refreshAccessToken();
+    api.setAccessToken(data.body['access_token']);
+  } else {
+    // If refresh token isn't supplied, we'll follow the code grant auth pattern to get user's permission.
     const authorizeURL = api.createAuthorizeURL([
       'playlist-read',
       'playlist-read-private',
@@ -158,22 +173,19 @@ async function auth({clientId, clientSecret, redirectUri, refreshToken}) {
       'user-library-read',
       'user-library-modify',
     ], 'spotify-importer-state-value');
-    console.log(`Copy and paste this to your browser '${authorizeURL}'. Follow the steps. Then copy the value of the 'code' query string.`);
+    console.log(`Copy and paste this to your browser '${authorizeURL}'. Follow the steps on Spotify website. Then copy the value of the 'code' query string.`);
 
     const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-    const authCode = await prompt('Enter code here: ');
+    const authCode = await prompt('Paste code here and press Enter: ');
     const data = await api.authorizationCodeGrant(authCode);
 
     refreshToken = data.body['refresh_token'];
     api.setAccessToken(data.body['access_token']);
     api.setRefreshToken(refreshToken);
 
-    console.log('Refresh token: ' + refreshToken);
-  } else {
-    api.setRefreshToken(refreshToken);
-    const data = await api.refreshAccessToken();
-    api.setAccessToken(data.body['access_token']);
+    console.log('Copy the refresh token below this line so you can re-run this script faster later\n' + refreshToken);
   }
+
   return api;
 }
 
